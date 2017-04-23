0271e64a3120df3bbd49ebfb2827642d9942bb5c
Ubuntu <ubuntu@ip-172-31-28-184.us-west-2.compute.internal>
2017-04-23 23:23:14 +0000

    added addFollowing and createUser methods

--- GeekInRest/userViews.py
+++ GeekInRest/userViews.py
@@ -1,5 +1,6 @@
 #Created by Yi Li and Yiteng Xu in 2017/04/23
 #Modified by Yi Li, Yiteng Xu
+#A view where contains all user-relate interfaces (user follow, fetch user profiles, get followers, etc)
 
 from django.shortcuts import render
 from GeekInRest.models import Users, Posts, UserTags, Tags
@@ -15,8 +16,34 @@ import base64
 
 from django.utils import timezone
 
+
+
 #Add a following relationship
 @csrf_exempt
 def addFollowing(request):
     json_str = ((request.body).decode('utf-8'))
     body = json.loads(json_str)
+
+
+#Sign up a new user
+@csrf_exempt
+def createUser(request):
+    json_str = ((request.body).decode('utf-8'))
+    body = json.loads(json_str)
+    email = body['email']
+    userPassJudge = Users.objects.filter(email=email)
+    # print(userPassJudge.get(UserName=username).User_ID)
+
+    if userPassJudge:
+        return JsonResponse({'result': "false"})
+    else:
+        filedir = os.getcwd() + "/users/"
+        os.makedirs(os.path.dirname(filedir), exist_ok=True)
+        for image in images:
+            # filename = os.path.join(dir, "/posts/"+str_time +"/"+ str(i) + ".jpg")
+            with open(filedir + email + ".jpg", 'wb') as f:
+                f.write(base64.b64decode(image))
+            i = i + 1
+        data = Users(email=body["email"],password=body["password"],photo=filedir + email + ".jpg",username=body["username"])
+        data.save()
+        return JsonResponse({'result': "true"})
--- GeekInRest/views.py
+++ GeekInRest/views.py
@@ -45,32 +45,6 @@ def login(request):
     else:
         return JsonResponse({'result': False})
 
-#Sign up a new user
-@csrf_exempt
-def createUser(request):
-    json_str = ((request.body).decode('utf-8'))
-    body = json.loads(json_str)
-    email = body['email']
-    userPassJudge = Users.objects.filter(email=email)
-    # print(userPassJudge.get(UserName=username).User_ID)
-
-    if userPassJudge:
-        return JsonResponse({'result': "false"})
-    else:
-        filedir = os.getcwd() + "/users/"
-        os.makedirs(os.path.dirname(filedir), exist_ok=True)
-        for image in images:
-            # filename = os.path.join(dir, "/posts/"+str_time +"/"+ str(i) + ".jpg")
-            with open(filedir + email + ".jpg", 'wb') as f:
-                f.write(base64.b64decode(image))
-            i = i + 1
-
-        data = Users(email=body["email"],password=body["password"],photo=filedir + email + ".jpg",username=body["username"])
-        data.save()
-
-        return JsonResponse({'result': "true"})
-
-
 @csrf_exempt
 def user_tags(request):
     json_str = ((request.body).decode('utf-8'))
