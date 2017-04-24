3ae6e278a692ce80afa910929e68f32701b2098a
Ubuntu <ubuntu@ip-172-31-28-184.us-west-2.compute.internal>
2017-04-24 00:19:03 +0000

    added a new view in url.py

--- GeekInProject/urls.py
+++ GeekInProject/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers
 from GeekInRest import views
+from GeekInRest import userViews
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
